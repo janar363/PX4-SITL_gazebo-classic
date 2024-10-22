099775fb5e2f75d99b2eee6ec7e02aeb1e229cf3
Bohan Zhang <bohan.zhang.1@slu.edu>
2024-10-22 15:27:29 -0500

    Update: experiment 30 m/s

--- src/gazebo_wind_plugin.cpp
+++ src/gazebo_wind_plugin.cpp
@@ -187,7 +187,7 @@ namespace gazebo {
                 }
 
                 WindDataProcessor::WindVal windVal = arr.getCubeWindValue(i, position.X(), position.Y(), position.Z());
-                windValues[i] = ignition::math::Vector3d(10, 0, 0);
+                windValues[i] = ignition::math::Vector3d(30, 0, 0);
 
                 // air density at sea level at 15 degree C = 1.225 kg/m^3
                 // double airDensity = 1.225;
